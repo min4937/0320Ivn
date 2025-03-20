8427b132d54c1b26f86bb6f3799230f05c4d45b2
min4937 <pmy1504@naver.com>
2025-03-21 00:44:38 +0900

    Feat: UIManager에 3개의 UI를 연결

---
+++ Assets/Scenes/4.Scripts/UIManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private UIMainMenu uiMainMenu;  
+    [SerializeField] private UIStatus uiStatus;       
+    [SerializeField] private UIInventory uiInventory;
+
+    
+}
